f5f68271b4aa72c2dd65b295f18b2d3df772b86f
Priyansh-911 <priyansh91103@gmail.com>
2023-04-24 21:14:52 +0530

    commit #1

--- training.py
+++ training.py
@@ -5,5 +5,27 @@ import numpy as np
 
 
 import nltk
+import tensorflow
+from keras.models import Sequential
+from keras.layers import Dense, activation, Dropout
+from keras.optimizers import SGD
 from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import 
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['!', ',', '.', '?']
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+print(documents)
