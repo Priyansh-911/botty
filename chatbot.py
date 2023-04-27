cd4a403f228d0004d54129e97db14c4f73cd96ed
Priyansh-911 <priyansh91103@gmail.com>
2023-04-27 19:23:25 +0530

    init chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
