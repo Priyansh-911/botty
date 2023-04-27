dbffbdc19cda4e426703c26b0cc333d20ac5ec2e
Priyansh-911 <priyansh91103@gmail.com>
2023-04-27 23:07:39 +0530

    flask server setup and making template

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from chatbot import getResponse, predictClass
 
-app = Flask()
-
-@app.route('/')
-def chat():
-        print("Hello World!")
+intents = json.loads(open('intents.json').read())
+app = Flask(__name__)
 
 
+@app.route('/chat', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        message = request.form['message']
+        # ints = predictClass(message)
+        # res = getResponse(ints, intents)
+        # return render_template('index,html', message=message, response=res)
 
-if __name__ == __main__:
-        app.run(debug=True)
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
