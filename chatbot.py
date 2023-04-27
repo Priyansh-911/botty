8c615999de355d175292802774b11aeb7e240fb8
Priyansh-911 <priyansh91103@gmail.com>
2023-04-27 20:00:31 +0530

    Chatbot application update

--- chatbot.py
+++ chatbot.py
@@ -12,3 +12,49 @@ lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentenceWords = nltk.word_tokenize(sentence)
+    sentenceWords = [lemmatizer.lemmatize(word) for word in sentenceWords]
+    return sentenceWords
+
+def bagOfWords(sentence):
+    sentenceWords = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentenceWords:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predictClass(sentence):
+    bow = bagOfWords(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    result = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    result.sort(key=lambda x : x[1], reverse=True)
+    returnList = []
+    for r in result:
+        returnList.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return returnList
+
+def getResponse(intentsList, intentsJSON):
+    tag = intentsList[0]['intent']
+    listOfIntents = intentsJSON['intents']
+    for i in listOfIntents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+print("Bot is up and Running!!!!")
+
+while True:
+    message = input("")
+    ints = predictClass(message)
+    res = getResponse(ints, intents)
+    print(res)
--- training.py
+++ training.py
@@ -32,7 +32,7 @@ words = sorted(set(words))
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
@@ -65,7 +65,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='CategoricalCrossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 
-model.save('chatbot_model.model')
+model.save('chatbot_model.h5', hist)
 print("Done")
