38b7a501ca947a15febf8dc18bf2d55ca97c9b8a
Priyansh-911 <priyansh91103@gmail.com>
2023-04-27 20:24:28 +0530

    bug fixes

--- chatbot.py
+++ chatbot.py
@@ -13,12 +13,14 @@ intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.model')
+model = load_model('chatbot_model.h5')
+
 
 def clean_up_sentence(sentence):
     sentenceWords = nltk.word_tokenize(sentence)
     sentenceWords = [lemmatizer.lemmatize(word) for word in sentenceWords]
     return sentenceWords
+
 
 def bagOfWords(sentence):
     sentenceWords = clean_up_sentence(sentence)
@@ -29,17 +31,19 @@ def bagOfWords(sentence):
                 bag[i] = 1
     return np.array(bag)
 
+
 def predictClass(sentence):
     bow = bagOfWords(sentence)
-    res = model.predict(np.array([bow]))[0]
+    resus = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
-    result = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    result = [[i, r] for i, r in enumerate(resus) if r > ERROR_THRESHOLD]
 
-    result.sort(key=lambda x : x[1], reverse=True)
+    result.sort(key=lambda x: x[1], reverse=True)
     returnList = []
     for r in result:
         returnList.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return returnList
+
 
 def getResponse(intentsList, intentsJSON):
     tag = intentsList[0]['intent']
