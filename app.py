94ed9504b46a2cff8ee4a0b4b8dbc8ddbe421e03
Priyansh-911 <priyansh91103@gmail.com>
2023-04-29 02:03:52 +0530

    integration of frontend with backend #1

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ intents = json.loads(open('intents.json').read())
 app = Flask(__name__)
 
 
-@app.route('/chat', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chatbot2.html', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         message = request.form['message']
         ints = predictClass(message)
         res = getResponse(ints, intents)
-        return render_template('index.html', message=message, response=res)
+        return render_template('chatbot2.html', message=message, response=res)
 
     else:
-        return render_template('index.html')
+        return render_template('chatbot2.html')
 
 
 if __name__ == '__main__':
